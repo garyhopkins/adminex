f29f3d6cdeeac76110961712f6ed7d01edf41e9a
Gary Hopkins <gary@verbidio.com>
2025-06-27 14:49:54 -0500

    Added app.go

---
+++ backend/app/app.go
@@ -0,0 +1,53 @@
+package app
+
+import (
+	"context"
+	"log"
+
+	"github.com/garyhopkins/adminex/repository/ent/entgen"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type AppSettings struct {
+	EntClient *entgen.Client
+}
+
+var CoreApp *AppSettings
+
+func setupApp() {
+	ctx := context.Background()
+
+	// set up a db client. testing for now
+	client := NewTestDbClient()
+	RunDbMigrations(ctx, client)
+
+	a := AppSettings{
+		EntClient: client,
+	}
+	CoreApp = &a
+}
+
+func NewTestDbClient() *entgen.Client {
+	client, err := entgen.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+	return client
+}
+
+func RunDbMigrations(ctx context.Context, client *entgen.Client) {
+	if err := client.Schema.Create(ctx); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+}
+
+func GetApp() *AppSettings {
+	if CoreApp == nil {
+		setupApp()
+	}
+	return CoreApp
+}
